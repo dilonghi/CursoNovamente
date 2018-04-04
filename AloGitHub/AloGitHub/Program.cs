a4c0742f3010ca9dc141af2cad25455e5056a3e0
dilonghi <diego@ibservi.com.br>
2018-04-03 21:09:57 -0300

    Criação do Proejto

---
+++ AloGitHub/AloGitHub/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AloGitHub
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Ola Mundo");
+        }
+    }
+}
